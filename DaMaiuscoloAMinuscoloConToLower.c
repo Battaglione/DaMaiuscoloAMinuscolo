dff7c57e787080944a96bb678a604dc0f5da7c2f
Azel04 <75199595+Azel04@users.noreply.github.com>
2021-01-15 09:29:09 +0100

    Create DaMaiuscoloAMinuscoloConToLower.c

---
+++ DaMaiuscoloAMinuscoloConToLower.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#define MAX 80
+
+int main (){
+    char s[MAX], m[MAX];
+    int i;
+    do{
+        printf("Inserire un nome: ");
+        gets(s);
+    }while(strlen(s) > MAX);
+
+    strcpy(m,s);
+
+    for(i = 0; i < strlen(m); i++){
+        m[i] = tolower(m[i]);
+    }
+    
+    printf("Il nome finale e' ");
+    puts(m);
+}
